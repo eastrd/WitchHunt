f96a83623f556ff0314abf7bb02da84781a7bca5
eastrd <yjia285@student.monash.edu>
2018-01-11 17:06:01 +1100

    Changed flask to run under __name__ == __main__

--- run.py
+++ run.py
@@ -68,4 +68,5 @@ def apply_caching(response):
     return response
 
 
-app.run(host="0.0.0.0", debug=False, port=80)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=False, port=12345)
