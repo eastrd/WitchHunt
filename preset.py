8dd32c1a94d98e09d4d51596a184662a587c5d95
eastrd <yjia285@student.monash.edu>
2018-01-15 15:20:04 +1100

    Inited for storing HTML templates

---
+++ preset.py
@@ -0,0 +1,7 @@
+'''
+Preset module that contains all static HTML code for different default templates
+'''
+
+
+HTML_500 = '''<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 3.2 Final//EN"><title>500 Internal Server Error</title><h1>Internal Server Error</h1><p>The server encountered an internal error and was unable to complete your request.  Either the server is overloaded or there is an error in the application.</p>'''
+HTML_400 = '''<html><head><title>404 Not Found</title></head><body><h1>Not Found</h1>The requested URL was not found on this server.</body></html>'''
