b28fae8b72a6382447a967f4042b71ee8d6f9091
eastrd <yjia285@student.monash.edu>
2018-02-11 10:48:43 +1100

    Modified existing templates & Added rules for new templates

--- preset.py
+++ preset.py
@@ -1,7 +1,8 @@
 '''
 Preset module that contains all static HTML code for different default templates
+[!] HTML templates need to be wrapped inside <html> </html> tags
 '''
 
 
-HTML_500 = '''<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 3.2 Final//EN"><title>500 Internal Server Error</title><h1>Internal Server Error</h1><p>The server encountered an internal error and was unable to complete your request.  Either the server is overloaded or there is an error in the application.</p>'''
+HTML_500 = '''<html><title>500 Internal Server Error</title><h1>Internal Server Error</h1><p>The server encountered an internal error and was unable to complete your request.  Either the server is overloaded or there is an error in the application.</p></html>'''
 HTML_400 = '''<html><head><title>404 Not Found</title></head><body><h1>Not Found</h1>The requested URL was not found on this server.</body></html>'''
